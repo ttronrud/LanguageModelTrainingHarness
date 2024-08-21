41c33130071879a70c4de8aa5c0ab6b0498461ca
Thor Tronrud <ttronrud@starfishmedical.com>
2024-08-21 12:49:56 -0700

    Improved training data generation to combine multiple datasets

--- training_utils.py
+++ training_utils.py
@@ -137,7 +137,7 @@ def generate_training_data(dset = "HuggingFaceTB/smollm-corpus", dset_name = Non
     if not os.path.exists("cache"):
         os.mkdir("cache")
     enc = tiktoken.get_encoding("gpt2")
-    dataset = load_dataset(dset,dset_name, cache_dir = "cache", split = "train", streaming = True, num_proc=8)
+    dataset = load_dataset(dset,dset_name, cache_dir = "cache", split = "train", streaming = True)
     dataset = iter(DataLoader(dataset, num_workers = 8, batch_size = 512, 
         collate_fn = lambda batch: default_collate([{"text":e.pop(text_col)} for e in batch])
         ))
@@ -183,5 +183,86 @@ def generate_training_data(dset = "HuggingFaceTB/smollm-corpus", dset_name = Non
             if tot_tok > targtok:
                 break
         enable_progress_bar()
+    
+    arr.flush()
+    
+"""
+Operates the same as single-source training data generation
+method, however it incorporates set counts of tokens from
+multiple datasets, allowing for the construction of
+composite training files.
+"""     
+def generate_training_data_mix(dset = ["HuggingFaceTB/smollm-corpus","togethercomputer/RedPajama-Data-1T"], 
+                               dset_name = ["fineweb-edu-dedup", "arxiv"], 
+                               text_col = ["text", "text"],
+                               tokens_to_save = [1e10, 1e10],
+                               senc = "gpt2", num_proc = 1,
+                               out_fname = "train.bin", overwrite = False):
+    global enc
+    
+    enable_progress_bar()
+    # leave if data binary already exists
+    if os.path.isfile(out_fname) and overwrite == False:
+        print(f"output file {out_fname} already exists. Set 'overwrite = True' to regenerate.")
+        return
+    if not os.path.exists("cache"):
+        os.mkdir("cache")
+    enc = tiktoken.get_encoding("gpt2")
+    
+    tot_tok = np.uint64(0)
+    reopentok_inc = np.uint64(5e8)
+    reopentok = reopentok_inc
+    end_tok = np.sum(tokens_to_save,dtype=np.uint64)
+    
+    dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+    arr = np.memmap(out_fname, dtype=dtype, mode='w+', shape=(int(end_tok),))
+    
+    for ds, ds_n, tc, ntok in zip(dset, dset_name, text_col, tokens_to_save):
+        dataset = load_dataset(ds,ds_n, cache_dir = "cache", split = "train", streaming = True)
+        dataset = iter(DataLoader(dataset, num_workers = 4, batch_size = 512, 
+            collate_fn = lambda batch: default_collate([{"text":e.pop(tc)} for e in batch])
+            ))
+    
+    
+        tot_tok_ds = np.uint64(0)
+        targtok = np.uint64(ntok)
+        running = True
+        
+        # have to disable progress bar here so terminal/cell isn't spammed
+        # AND THERE'S NO 'verbose=False' OPTION
+        disable_progress_bar()
+        with tqdm(desc="Tokens processed", total = targtok, position=0, leave=True) as pbar:
+            dsets = []
+            while running:
+                if tot_tok > reopentok:
+                    arr = np.memmap(out_fname, dtype=dtype, mode='r+')
+                    reopened = True
+                    reopentok += reopentok_inc
+
+                dat = Dataset.from_dict(next(dataset))
+                interim = dat.map(
+                    process,
+                    remove_columns=[tc],
+                    desc="tokenizing the splits",
+                    num_proc=num_proc
+                )
+                nu = np.sum(interim['len'], dtype=np.uint64)
+                if tot_tok + nu >= end_tok:
+                    arr[tot_tok:] = np.concatenate(interim['ids'])[:end_tok - tot_tok]
+                    nu = end_tok - tot_tok
+                elif tot_tok_ds + nu > targtok:
+                    arr[tot_tok:tot_tok + (targtok - tot_tok_ds)] = np.concatenate(interim['ids'])[:targtok - tot_tok_ds]
+                    nu = targtok - tot_tok_ds
+                elif tot_tok + nu < end_tok:
+                    arr[tot_tok:tot_tok+nu] = np.concatenate(interim['ids'])
+                
+                arr.flush()
+                
+                tot_tok += nu
+                tot_tok_ds += nu
+                pbar.update(nu)
+                if tot_tok_ds > targtok:
+                    break
+            enable_progress_bar()
     
     arr.flush()
